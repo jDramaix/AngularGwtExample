18eb20a84e6acdaa6d303a7b27365fc263748d62
Julien Dramaix <dramaix@google.com>
2015-11-09 06:54:11 +0100

    cleanup

--- src/main/java/jdramaix/polymer/client/component/HTMLAudioElement.java
+++
@@ -1,9 +0,0 @@
-package jdramaix.polymer.client.component;
-
-import jsinterop.annotations.JsPackage;
-import jsinterop.annotations.JsType;
-
-@JsType(isNative = true, namespace = JsPackage.GLOBAL)
-public abstract class HTMLAudioElement {
-  public native String canPlayType(String mediaType);
-}
